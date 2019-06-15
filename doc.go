76f7b793017adc31a19d3534ee98eb9e778d0f9e
lucas <lokstory@gmail.com>
2019-06-15 19:53:13 +0800

    update doc.go

--- doc.go
+++ doc.go
@@ -2,3 +2,8 @@ package main
 
 // Dependency Injection by annotations for Golang
 // https://github.com/lokstory/digo
+
+import (
+	_ "github.com/lokstory/digo/generator"
+	_ "github.com/lokstory/digo/model"
+)
